dd044ddf5a921ada16d30ddfcba4b3608f438487
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-01-29 11:29:25 -0300

    feat: Override Equals method to compare the actual value object for equality

--- ServiceContracts/DTO/CountryResponse.cs
+++ ServiceContracts/DTO/CountryResponse.cs
@@ -10,10 +10,35 @@ namespace ServiceContracts.DTO
     {
         public Guid CountryID { get; set; }
         public string? CountryName { get; set; }
+
+        // It compares the current object with another CountryResponse object for equality.
+        public override bool Equals(object? obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if (obj is not CountryResponse)
+            {
+                return false;
+            }
+
+            CountryResponse country_to_compare = (CountryResponse)obj;
+
+            return CountryID == country_to_compare.CountryID &&
+                   CountryName == country_to_compare.CountryName;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 
     public static class  CountryExtensions
     {
+        // Extension method to convert Country entity to CountryResponse DTO
         public static CountryResponse ToCountryResponse(this Country country)
         {
             return new CountryResponse()
