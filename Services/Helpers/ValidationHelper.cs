1c2386f0806d1b5c78883baf642f8e586dc39940
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-01-29 15:48:42 -0300

    feat: Add ValidationHelper model to use in any service

---
+++ Services/Helpers/ValidationHelper.cs
@@ -0,0 +1,22 @@
+﻿using ServiceContracts.DTO;
+using System.ComponentModel.DataAnnotations;
+
+namespace Services.Helpers
+{
+    public class ValidationHelper
+    {
+        internal static void ModelValidation(object obj)
+        {
+            // Validate all properties of "obj"
+            ValidationContext validationContext = new ValidationContext(obj);
+            List<ValidationResult> validationResults = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(obj, validationContext, validationResults, true);
+            if (!isValid)
+            {
+                throw new ArgumentException(validationResults.FirstOrDefault()?.ErrorMessage);
+            }
+
+        }
+    }
+}
