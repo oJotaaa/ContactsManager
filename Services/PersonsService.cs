a61f782536d0980a6f502e4be9a63281481ca2a4
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-01-29 15:32:12 -0300

    feat: Add implementation logic to AddPerson method

---
+++ Services/PersonsService.cs
@@ -0,0 +1,60 @@
+﻿using Entities;
+using ServiceContracts;
+using ServiceContracts.DTO;
+using System.Transactions;
+
+namespace Services
+{
+    public class PersonsService : IPersonsService
+    {
+        // Private fields
+        private readonly List<Person> _persons;
+        private readonly ICountriesService _countriesService;
+
+        // Constructor
+        public PersonsService()
+        {
+            _persons = new List<Person>();
+            _countriesService = new CountriesService();
+        }
+
+        private PersonResponse ConvertPersonIntoPersonResponse(Person person)
+        {
+            PersonResponse personResponse = person.ToPersonResponse();
+            personResponse.Country = _countriesService.GetCountryByCountryID(person.CountryID)?.CountryName;
+            return personResponse;
+        }
+
+        public PersonResponse AddPerson(PersonAddRequest? personAddRequest)
+        {
+            // Check if "personAddRequest" is not null
+            if (personAddRequest == null)
+            {
+                throw new ArgumentNullException(nameof(personAddRequest), "PersonAddRequest cannot be null");
+            }
+
+            // Validate all properties of "personAddRequest"
+            if (string.IsNullOrEmpty(personAddRequest.PersonName))
+            {
+                throw new ArgumentException("PersonName can't be blank");
+            }
+
+            // Convert "personAddRequest" to "Person" object
+            Person person = personAddRequest.ToPerson();
+
+            // Generate a new GUID for PersonID
+            person.PersonID = Guid.NewGuid();
+
+            // Then add it into List<Persons>
+            _persons.Add(person);
+
+            // Return PersonResponse object with generated PersonID and other details
+            return ConvertPersonIntoPersonResponse(person);
+        }
+
+        public List<PersonResponse> GetAllPersons()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
