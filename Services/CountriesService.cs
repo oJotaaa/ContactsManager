4e0504a933fd8518f4746442b60a31292e4e3af1
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-01-29 10:08:55 -0300

    feat(CountriesServices): Adds all validation and add logic

--- Services/CountriesService.cs
+++ Services/CountriesService.cs
@@ -1,11 +1,52 @@
-﻿using ServiceContracts;
+﻿using Entities;
+using ServiceContracts;
 using ServiceContracts.DTO;
 
 namespace Services
 {
     public class CountriesService : ICountriesService
     {
+        private readonly List<Country> _countries;
+
+        public CountriesService()
+        {
+            _countries = new List<Country>();
+        }
+
         public CountryResponse AddCountry(CountryAddRequest? countryAddRequest)
+        {
+            // Validation: Check if countryAddRequest is null
+            if (countryAddRequest == null)
+            {
+                throw new ArgumentNullException(nameof(countryAddRequest));
+            }
+
+            // Validation: Check if CountryName is null or empty
+            if (string.IsNullOrEmpty(countryAddRequest.CountryName))
+            {
+                throw new ArgumentException(nameof(countryAddRequest.CountryName));
+            }
+
+            // Validation: Check for duplicate CountryName (case-insensitive)
+            if (_countries.Where(temp => temp.CountryName == countryAddRequest.CountryName).Count() > 0)
+            {
+                throw new ArgumentException("Given country name already exists");
+            }
+
+            // Convert object from CountryAddRequest to Country type
+            Country country = countryAddRequest.ToCountry();
+
+            // Generate CountryID
+            country.Id = Guid.NewGuid();
+
+            // Add country to the _countries list
+            _countries.Add(country);
+
+            return country.ToCountryResponse();
+
+        }
+
+        public List<CountryResponse> GetAllCountries()
         {
             throw new NotImplementedException();
         }
