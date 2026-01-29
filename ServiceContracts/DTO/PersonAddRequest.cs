2b3fe5c271dba1aeaa49c053320ab04502c65c25
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-01-29 14:23:06 -0300

    feat: Add Person DTOs

---
+++ ServiceContracts/DTO/PersonAddRequest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Entities;
+using ServiceContracts.Enums;
+
+namespace ServiceContracts.DTO
+{
+    /// <summary>
+    /// Acts as a Data Transfer Object (DTO) for adding a new person.
+    /// </summary>
+    public class PersonAddRequest
+    {
+        public string? PersonName { get; set; }
+        public string? Email { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public GenderOptions? Gender { get; set; }
+        public Guid? CountryID { get; set; }
+        public string? Address { get; set; }
+        public bool ReceiveNewsLetters { get; set; }
+
+        /// <summary>
+        /// Creates a new Person instance populated with the current object's data.
+        /// </summary>
+        /// <returns>A Person object containing the name, email, date of birth, gender, address, and newsletter preference from
+        /// the current instance.</returns>
+        public Person ToPerson()
+        {
+            return new Person()
+            {
+                PersonName = PersonName,
+                Email = Email,
+                DateOfBirth = DateOfBirth,
+                Gender = Gender.ToString(),
+                Address = Address,
+                ReceiveNewsLetters = ReceiveNewsLetters
+            };
+        }
+    }
+}
---
+++ ServiceContracts/DTO/PersonResponse.cs
@@ -0,0 +1,73 @@
+﻿using Entities;
+
+namespace ServiceContracts.DTO
+{
+    /// <summary>
+    /// Represents DTO class that is used as return type of most methods of Persons Service.
+    /// </summary>
+    public class PersonResponse
+    {
+        public Guid PersonID { get; set; }
+        public string? PersonName { get; set; }
+        public string? Email { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string? Gender { get; set; }
+        public Guid? CountryID { get; set; }
+        public string? Country { get; set; }
+        public string? Address { get; set; }
+        public bool ReceiveNewsLetters { get; set; }
+        public double? Age { get; set; }
+
+        /// <summary>
+        /// Determines whether the specified object is equal to the current PersonResponse instance.
+        /// </summary>
+        /// <param name="obj">The object to compare with the current PersonResponse instance.</param>
+        /// <returns>true if the specified object is a PersonResponse and has the same properties as the current instance;
+        /// otherwise, false.</returns>
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+
+            if (obj.GetType() != typeof(PersonResponse)) return false;
+
+            PersonResponse person = (PersonResponse)obj;
+            return PersonID == person.PersonID &&
+                   PersonName == person.PersonName &&
+                   Email == person.Email &&
+                   DateOfBirth == person.DateOfBirth &&
+                   Gender == person.Gender &&
+                   CountryID == person.CountryID &&
+                   Address == person.Address &&
+                   ReceiveNewsLetters == person.ReceiveNewsLetters;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+
+    public static class PersonExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="person"></param>
+        /// <returns></returns>
+        public static PersonResponse ToPersonResponse(this Person person)
+        {
+            return new PersonResponse()
+            {
+                PersonID = person.PersonID,
+                PersonName = person.PersonName,
+                Email = person.Email,
+                DateOfBirth = person.DateOfBirth,
+                Gender = person.Gender,
+                CountryID = person.CountryID,
+                Address = person.Address,
+                ReceiveNewsLetters = person.ReceiveNewsLetters,
+                Age = (person.DateOfBirth != null) ? Math.Round((DateTime.Now - person.DateOfBirth.Value).TotalDays / 365.25) : null
+            };
+        }
+    }
+}
