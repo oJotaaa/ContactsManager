317ef78bd6519cdb075e9bea7bc583c45d8444b9
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-01-29 15:11:30 -0300

    feat: Add Person interface with methods

---
+++ ServiceContracts/IPersonsService.cs
@@ -0,0 +1,25 @@
+﻿using ServiceContracts.DTO;
+
+namespace ServiceContracts
+{
+    /// <summary>
+    /// Defines operations for managing person records, including adding new persons and retrieving all existing
+    /// persons.
+    /// </summary>
+    public interface IPersonsService
+    {
+        /// <summary>
+        /// Adds a new person into the list of persons. 
+        /// </summary>
+        /// <param name="personAddRequest">Person to Add</param>
+        /// <returns>Returns the same person details, along with newly generated PersonID</returns>
+        PersonResponse AddPerson(PersonAddRequest? personAddRequest);
+
+        /// <summary>
+        /// Returns all persons
+        /// </summary>
+        /// <returns>Return a list of objects of PersonResponse</returns>
+        List<PersonResponse> GetAllPersons();
+
+    }
+}
