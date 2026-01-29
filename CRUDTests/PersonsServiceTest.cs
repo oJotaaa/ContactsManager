964c43e254955d2bb81fc69a0bb485448ab40351
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-01-29 16:12:36 -0300

    feat: Add tests to the GetPersonByPersonID method

--- CRUDTests/PersonsServiceTest.cs
+++ CRUDTests/PersonsServiceTest.cs
@@ -10,11 +10,13 @@ namespace CRUDTests
     {
         // Private fields
         private readonly IPersonsService _personService;
+        private readonly ICountriesService _countriesService;
 
         // Constructor
         public PersonsServiceTest()
         {
             _personService = new PersonsService();
+            _countriesService = new CountriesService();
         }
 
         #region AddPersonTests
@@ -65,6 +67,50 @@ namespace CRUDTests
             // Assert
             Assert.True(personResponseFromAdd.PersonID != Guid.Empty);
             Assert.Contains(personResponseFromAdd, personsList);
+        }
+        #endregion
+
+        #region GetPersonByPersonIDTests
+
+        // If we supply null as PersonID, it should return null as PersonResponse
+        [Fact]
+        public void GetPersonByPersonID_NullPersonID()
+        {
+            // Arrange
+            Guid? personID = null;
+
+            // Act
+            PersonResponse? personResponse = _personService.GetPersonByPersonID(personID);
+
+            // Assert
+            Assert.Null(personResponse);
+        }
+
+        // If we supply a valid person id, it should return the corresponding Person object
+        [Fact]
+        public void GetPersonByPersonID_WithPersonID()
+        {
+            // Arrange
+            CountryAddRequest countryAddRequest = new CountryAddRequest() { CountryName = "Canada" };
+            CountryResponse countryResponse = _countriesService.AddCountry(countryAddRequest);
+
+            // Act
+            PersonAddRequest person_request = new PersonAddRequest()
+            {
+                PersonName = "John Doe",
+                Email = "person@sample.com",
+                Address = "Some address",
+                CountryID = countryResponse.CountryID,
+                DateOfBirth = DateTime.Parse("1990-01-01"),
+                Gender = GenderOptions.Male,
+                ReceiveNewsLetters = false
+            };
+            PersonResponse personResponseFromAdd = _personService.AddPerson(person_request);
+
+            PersonResponse? personResponse = _personService.GetPersonByPersonID(personResponseFromAdd.PersonID);
+
+            // Assert
+            Assert.Equal(personResponseFromAdd, personResponse);
         }
         #endregion
     }
