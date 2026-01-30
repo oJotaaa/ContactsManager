ed209fbe59685bf468e165c3c4042c9e9aea0161
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-01-30 10:18:13 -0300

    feat: Add PersonUpdateRequest DTO

---
+++ ServiceContracts/DTO/PersonUpdateRequest.cs
@@ -0,0 +1,47 @@
+﻿using Entities;
+using ServiceContracts.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace ServiceContracts.DTO
+{
+    public class PersonUpdateRequest
+    {
+        /// <summary>
+        /// Acts as a Data Transfer Object (DTO) for update a person.
+        /// </summary>
+        [Required(ErrorMessage = "PersonID can't be blank")]
+        public Guid PersonID { get; set; }
+
+        [Required(ErrorMessage = "Person Name can't be blank")]
+        public string? PersonName { get; set; }
+
+        [Required(ErrorMessage = "Email can't be blank")]
+        [EmailAddress(ErrorMessage = "Email value should be a valid email")]
+        public string? Email { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public GenderOptions? Gender { get; set; }
+        public Guid? CountryID { get; set; }
+        public string? Address { get; set; }
+        public bool ReceiveNewsLetters { get; set; }
+
+        /// <summary>
+        /// Creates a new Person instance populated with the current object's data.
+        /// </summary>
+        /// <returns>A Person object containing the id, name, email, date of birth, gender, address, and newsletter preference from
+        /// the current instance.</returns>
+        public Person ToPerson()
+        {
+            return new Person()
+            {
+                PersonID = PersonID,
+                PersonName = PersonName,
+                Email = Email,
+                DateOfBirth = DateOfBirth,
+                Gender = Gender.ToString(),
+                CountryID = CountryID,
+                Address = Address,
+                ReceiveNewsLetters = ReceiveNewsLetters
+            };
+        }
+    }
+}
