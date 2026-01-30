a35ecc3f4dddd8961985d2c1efc3fe79bb873ecb
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-01-30 08:47:39 -0300

    feat: Add GetFilteredPersons method in the IPerson interface

--- ServiceContracts/IPersonsService.cs
+++ ServiceContracts/IPersonsService.cs
@@ -28,5 +28,16 @@ namespace ServiceContracts
         /// <returns>A matching Person Object containing the details of the person if found.</returns>
         PersonResponse? GetPersonByPersonID(Guid? personID);
 
+        /// <summary>
+        /// Retrieves a list of persons that match the specified search criteria.
+        /// </summary>
+        /// <param name="searchBy">The name of the property to search by, such as "Name" or "Email". If null or empty, all
+        /// persons are returned.</param>
+        /// <param name="searchString">The value to search for within the specified property. The search is typically case-insensitive and may
+        /// match partial values.</param>
+        /// <returns>A list of <see cref="PersonResponse"/> objects that match the search criteria. Returns an empty list if no
+        /// persons are found.</returns>
+        List<PersonResponse> GetFilteredPersons(string? searchBy, string searchString);
+
     }
 }
