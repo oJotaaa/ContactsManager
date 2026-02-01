ec16917d3f99f8f828b7e272c9d5ed0398d74236
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-02-01 14:17:16 -0300

    feat: Add Create Action to create new persons and use [action] route

--- ContactsManager/Controllers/PersonsController.cs
+++ ContactsManager/Controllers/PersonsController.cs
@@ -5,16 +5,19 @@ using ServiceContracts.Enums;
 
 namespace ContactsManager.Controllers
 {
+    [Route("[controller]")]
     public class PersonsController : Controller
     {
         private readonly IPersonsService _personsService;
+        private readonly ICountriesService _countriesService;
 
-        public PersonsController(IPersonsService personsService)
+        public PersonsController(IPersonsService personsService, ICountriesService countriesService)
         {
             _personsService = personsService;
+            _countriesService = countriesService;
         }
 
-        [Route("persons/index")]
+        [Route("[action]")]
         [Route("/")]
         public IActionResult Index(string searchBy, string? searchString, string sortBy = nameof(PersonResponse.PersonName), SortOrderOptions sortOrder = SortOrderOptions.ASC)
         {
@@ -39,6 +42,35 @@ namespace ContactsManager.Controllers
             ViewBag.CurrentSortOrder = sortOrder.ToString();
 
             return View(sortedPersons);
+        }
+
+        // Executes when the user clicks on "Create Person" hyperlink (while opening the create view)
+        [HttpGet]
+        [Route("[action]")]
+        public IActionResult Create()
+        {
+            List<CountryResponse> countries = _countriesService.GetAllCountries();
+            ViewBag.Countries = countries;
+            return View(); 
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult Create(PersonAddRequest personAddRequest)
+        {
+            if (!ModelState.IsValid)
+            {
+                List<CountryResponse> countries = _countriesService.GetAllCountries();
+                ViewBag.Countries = countries;
+                ViewBag.Errors = ModelState.Values.SelectMany(v => v.Errors).SelectMany(e => e.ErrorMessage).ToList();
+                return View();
+            }
+
+            // Call the service method
+            PersonResponse personResponse = _personsService.AddPerson(personAddRequest);
+
+            // Navigate to Index() action method
+            return RedirectToAction("Index", "Persons");
         }
     }
 }
