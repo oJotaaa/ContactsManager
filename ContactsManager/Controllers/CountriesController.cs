ce0ad62fd009d9ca8eabc9529761b6fcdb71cb1f
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-02-03 16:30:28 -0300

    feat: Add CountriesController with UploadFromExcel Action

---
+++ ContactsManager/Controllers/CountriesController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceContracts;
+
+namespace ContactsManager.Controllers
+{
+    [Route("[controller]")]
+    public class CountriesController : Controller
+    {
+        private readonly ICountriesService _countriesService;
+
+        public CountriesController(ICountriesService countriesService)
+        {
+            _countriesService = countriesService;
+        }
+
+
+        // Countries/UploadFromExcel
+        [HttpGet]
+        [Route("[action]")]
+        public IActionResult UploadFromExcel()
+        {
+            return View();
+        }
+
+        // Countries/UploadFromExcel
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> UploadFromExcel(IFormFile excelFile)
+        {
+            if (excelFile == null || excelFile.Length == 0)
+            {
+                ViewBag.ErrorMessage = "Please select an xlsx file";
+                return View();
+            }
+
+            if (!Path.GetExtension(excelFile.FileName).Equals(".xlsx", StringComparison.OrdinalIgnoreCase))
+            {
+                ViewBag.ErrorMessage = "Unsupported file. 'xlsx' expected";
+                return View();
+            }
+
+            int countriesCountInserted = await _countriesService.UploadCountriesFromExcelFile(excelFile);
+            ViewBag.Message = $"{countriesCountInserted} Countries Uploaded.";
+            return View();
+        }
+    }
+
+}
