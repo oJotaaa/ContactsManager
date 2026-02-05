a796c10d5c46d2fe9a964fc285b7180fbb354420
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-02-05 13:30:18 -0300

    feat: Define RepositoryContracts to ICountries and IPersons

---
+++ RepositoryContracts/ICountriesRepository.cs
@@ -0,0 +1,40 @@
+﻿using Entities;
+
+namespace RepositoryContracts
+{
+    /// <summary>
+    /// Represents data access logic for managing Person entity
+    /// </summary>
+    public interface ICountriesRepository
+    {
+        /// <summary>
+        /// Asynchronously adds a new country to the data store.
+        /// </summary>
+        /// <param name="country">The country to add. Cannot be null.</param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        Task<Country> AddCountry(Country country);
+
+        /// <summary>
+        /// Asynchronously retrieves a list of all available countries.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation. The task result contains a list of <see cref="Country"/>
+        /// objects representing all countries. The list will be empty if no countries are available.</returns>
+        Task<List<Country>> GetAllCountries();
+
+        /// <summary>
+        /// Asynchronously retrieves the country that matches the specified country identifier.
+        /// </summary>
+        /// <param name="countryID">The unique identifier of the country to retrieve.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the <see cref="Country"/> if
+        /// found; otherwise, <see langword="null"/>.</returns>
+        Task<Country?> GetCountryByCountryID(Guid countryID);
+
+        /// <summary>
+        /// Asynchronously retrieves a country that matches the specified country name.
+        /// </summary>
+        /// <param name="countryName">The name of the country to search for. Cannot be null or empty.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the matching <see
+        /// cref="Country"/> if found; otherwise, <see langword="null"/>.</returns>
+        Task<Country?> GetCountryByCountryName(string countryName);
+    }
+}
---
+++ RepositoryContracts/IPersonsRepository.cs
@@ -0,0 +1,60 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace RepositoryContracts
+{
+    /// <summary>
+    /// Defines a contract for accessing and managing person entities in a data store.
+    /// </summary>
+    public interface IPersonsRepository
+    {
+        /// <summary>
+        /// Asynchronously adds a new person to the data store.
+        /// </summary>
+        /// <param name="person">The person to add. Cannot be null.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the added person, including any
+        /// updated properties such as a generated identifier.</returns>
+        Task<Person> AddPerson(Person person);
+
+        /// <summary>
+        /// Asynchronously retrieves a list of all persons.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation. The task result contains a list of <see cref="Person"/>
+        /// objects representing all persons. The list will be empty if no persons are found.</returns>
+        Task<List<Person>> GetAllPersons();
+
+        /// <summary>
+        /// Asynchronously retrieves a person record that matches the specified unique identifier.
+        /// </summary>
+        /// <param name="personID">The unique identifier of the person to retrieve.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the <see cref="Person"/> object
+        /// if found; otherwise, <see langword="null"/>.</returns>
+        Task<Person?> GetPersonByPersonID(Guid personID);
+
+        /// <summary>
+        /// Returns all person objects based on the given expression
+        /// </summary>
+        /// <param name="predicate">LINQ expression to check</param>
+        /// <returns>All matching persons with given condition</returns>
+        Task<List<Person>> GetFilteredPersons(Expression<Func<Person, bool>> predicate);
+
+        /// <summary>
+        /// Deletes the person record that matches the specified unique identifier.
+        /// </summary>
+        /// <param name="personID">The unique identifier of the person to delete.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result is <see langword="true"/> if the person
+        /// was successfully deleted; otherwise, <see langword="false"/>.</returns>
+        Task<bool> DeletePersonByPersonID(Guid personID);
+
+        /// <summary>
+        /// Updates the details of an existing person asynchronously.
+        /// </summary>
+        /// <param name="person">The person object containing the updated information. The object's identifier must correspond to an existing
+        /// person. Cannot be null.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the updated person object.</returns>
+        Task<Person> UpdatePerson(Person person);
+    }
+}
