6e7a8de73add9c24c57e0d9a2248b0da430331f3
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-02-05 22:18:08 -0300

    feat: Add PersonsControllerIntegrationTest and CustomWebApplicationFactory to do tests

---
+++ CRUDTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,28 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace ContactsManagerTests
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            base.ConfigureWebHost(builder);
+
+            builder.UseEnvironment("Test");
+
+            builder.ConfigureServices(services =>
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("DatabaseForTesting");
+                });
+            });
+        }
+
+    }
+}
---
+++ CRUDTests/PersonsControllerIntegrationTest.cs
@@ -0,0 +1,40 @@
+﻿using Fizzler.Systems.HtmlAgilityPack;
+using FluentAssertions;
+using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactsManagerTests
+{
+    public class PersonsControllerIntegrationTest : IClassFixture<CustomWebApplicationFactory>
+    {
+        private readonly HttpClient _client;
+
+        public PersonsControllerIntegrationTest(CustomWebApplicationFactory factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        #region Index
+
+        [Fact]
+        public async Task Index_ToReturnView()
+        {
+            // Act
+            HttpResponseMessage response = await _client.GetAsync("/Persons/Index");
+
+            // Assert
+            response.IsSuccessStatusCode.Should().BeTrue();
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            HtmlDocument html = new HtmlDocument();
+            html.LoadHtml(responseBody);
+            var document = html.DocumentNode;
+
+            document.QuerySelectorAll("table.persons").Should().NotBeNull();
+        }
+        #endregion
+    }
+}
