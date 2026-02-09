8d32d8265beb8cf25b790d6678c44e6cdfe6f734
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-02-09 11:14:26 -0300

    feat: Inject ILogger and add logs to Index action

--- ContactsManager/Controllers/PersonsController.cs
+++ ContactsManager/Controllers/PersonsController.cs
@@ -12,17 +12,23 @@ namespace ContactsManager.Controllers
     {
         private readonly IPersonsService _personsService;
         private readonly ICountriesService _countriesService;
+        private readonly ILogger<PersonsController> _logger;
 
-        public PersonsController(IPersonsService personsService, ICountriesService countriesService)
+        public PersonsController(IPersonsService personsService, ICountriesService countriesService, ILogger<PersonsController> logger)
         {
             _personsService = personsService;
             _countriesService = countriesService;
+            _logger = logger;
         }
 
         [Route("[action]")]
         [Route("/")]
         public async Task<IActionResult> Index(string searchBy, string? searchString, string sortBy = nameof(PersonResponse.PersonName), SortOrderOptions sortOrder = SortOrderOptions.ASC)
         {
+            _logger.LogInformation("Index action method of PersonsController");
+
+            _logger.LogDebug($"searchBy: {searchBy}, searchString: {searchString}, sortBy: {sortBy}, sortOrder: {sortOrder}");
+
             // Search
             ViewBag.SearchFields = new Dictionary<string, string>()
             {
