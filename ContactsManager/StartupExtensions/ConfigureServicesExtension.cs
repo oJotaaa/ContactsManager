aa5f975f4b52b601c6e1bd1bfa9d96dcd352ff64
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-02-10 15:18:54 -0300

    feat: Add ConfigureServices extension method to services to add all services

---
+++ ContactsManager/StartupExtensions/ConfigureServicesExtension.cs
@@ -0,0 +1,40 @@
+﻿using ContactsManager.Filters.ActionFilters;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using Repositories;
+using RepositoryContracts;
+using ServiceContracts;
+using Services;
+
+namespace ContactsManager
+{
+    public static class ConfigureServicesExtension
+    {
+        public static IServiceCollection ConfigureServices(this IServiceCollection services, IConfiguration configuration, IWebHostEnvironment webHostEnvironment)
+        {
+            services.AddTransient<ResponseHeaderActionFilter>();
+            services.AddControllersWithViews();
+
+            // Add services into IoC container
+            services.AddScoped<ICountriesRepository, CountriesRepository>();
+            services.AddScoped<IPersonsRepository, PersonsRepository>();
+            services.AddScoped<ICountriesService, CountriesService>();
+            services.AddScoped<IPersonsService, PersonsService>();
+
+            if (webHostEnvironment.EnvironmentName != "Test")
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                {
+                    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+                });
+            }
+
+            services.AddHttpLogging(options =>
+            {
+                options.LoggingFields = Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.RequestProperties | Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.ResponseHeaders;
+            });
+
+            return services;
+        }
+    }
+}
