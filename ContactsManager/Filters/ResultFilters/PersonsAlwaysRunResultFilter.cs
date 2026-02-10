78c49017db83730e436e9344db9f0d7ce60b2552
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-02-10 15:16:13 -0300

    feat: Add PersonsAlwaysRunResultFilter filter

---
+++ ContactsManager/Filters/ResultFilters/PersonsAlwaysRunResultFilter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ContactsManager.Filters.ResultFilters
+{
+    public class PersonsAlwaysRunResultFilter : IAlwaysRunResultFilter
+    {
+        public void OnResultExecuted(ResultExecutedContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnResultExecuting(ResultExecutingContext context)
+        {
+            if (context.Filters.OfType<SkipFilter>().Any())
+            {
+                return;
+            }
+        }
+    }
+}
