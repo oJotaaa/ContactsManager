ca7adf091a468fa93fbb3adb4a54bfaae8363d9e
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-02-10 15:11:50 -0300

    feat: Add PersonCreateAndEditPostActionFilter filter to check data in model state with short-circuits

---
+++ ContactsManager/Filters/ActionFilters/PersonCreateAndEditPostActionFilter.cs
@@ -0,0 +1,46 @@
+﻿using ContactsManager.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using ServiceContracts;
+using ServiceContracts.DTO;
+
+namespace ContactsManager.Filters.ActionFilters
+{
+    public class PersonCreateAndEditPostActionFilter : IAsyncActionFilter
+    {
+        private readonly ICountriesService _countriesService;
+        private readonly ILogger<PersonCreateAndEditPostActionFilter> _logger;
+
+        public PersonCreateAndEditPostActionFilter(ICountriesService countriesService, ILogger<PersonCreateAndEditPostActionFilter> logger)
+        {
+            _countriesService = countriesService;
+            _logger = logger;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (context.Controller is PersonsController personsController)
+            {
+                if (!personsController.ModelState.IsValid)
+                {
+                    List<CountryResponse> countries = await _countriesService.GetAllCountries();
+                    personsController.ViewBag.Countries = countries;
+                    personsController.ViewBag.Errors = personsController.ModelState.Values.SelectMany(v => v.Errors).SelectMany(e => e.ErrorMessage).ToList();
+
+                    var personRequest = context.ActionArguments["personAddRequest"];
+                    context.Result = personsController.View(personRequest);
+                }
+                else
+                {
+                    await next();
+                }
+            }
+            else
+            {
+                await next();
+            }
+
+            // After Logic
+            _logger.LogInformation("In after logic of PersonsCreateAndEdit Action filter");
+        }
+    }
+}
