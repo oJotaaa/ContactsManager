cf5b38a8bed8d2397d0851c0f67e4e5b0ba2eae9
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-02-10 15:15:06 -0300

    feat: Add FeatureDisabledResourceFilter filter

---
+++ ContactsManager/Filters/ResourceFilter/FeatureDisabledResourceFilter.cs
@@ -0,0 +1,36 @@
+﻿using ContactsManager.Filters.ResultFilters;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ContactsManager.Filters.ResourceFilter
+{
+    public class FeatureDisabledResourceFilter : IAsyncResourceFilter
+    {
+        private readonly ILogger<FeatureDisabledResourceFilter> _logger;
+        private readonly bool _isDisabled;
+
+        public FeatureDisabledResourceFilter(ILogger<FeatureDisabledResourceFilter> logger, bool isDisabled = true)
+        {
+            _logger = logger;
+            _isDisabled = isDisabled;
+        }
+
+        public async Task OnResourceExecutionAsync(ResourceExecutingContext context, ResourceExecutionDelegate next)
+        {
+            // Log
+            _logger.LogInformation("{FilterName}.{MethodName} - before", nameof(FeatureDisabledResourceFilter), nameof(OnResourceExecutionAsync));
+
+            if (_isDisabled)
+            {
+                context.Result = new StatusCodeResult(501);
+            }
+            else
+            {
+                await next();
+            }
+
+            // Log
+            _logger.LogInformation("{FilterName}.{MethodName} - after", nameof(FeatureDisabledResourceFilter), nameof(OnResourceExecutionAsync));
+        }
+    }
+}
