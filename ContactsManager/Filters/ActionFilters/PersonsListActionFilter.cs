1ec1635162b0da2bda38fdc8a05729c1848a2ad6
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-02-10 15:13:28 -0300

    feat: Add PersonsListActionFilter filter to check arguments and give to action

---
+++ ContactsManager/Filters/ActionFilters/PersonsListActionFilter.cs
@@ -0,0 +1,103 @@
+﻿using ContactsManager.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using ServiceContracts.DTO;
+using ServiceContracts.Enums;
+
+namespace ContactsManager.Filters.ActionFilters
+{
+    public class PersonsListActionFilter : IActionFilter
+    {
+        private readonly ILogger<PersonsListActionFilter> _logger;
+        
+        public PersonsListActionFilter(ILogger<PersonsListActionFilter> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            // Log
+            _logger.LogInformation("{FilterName}.{MethodName} method", nameof(PersonsListActionFilter), nameof(OnActionExecuted));
+
+            PersonsController personsController = (PersonsController)context.Controller;
+
+            IDictionary<string, object?>? parameters = (IDictionary<string, object?>?)context.HttpContext.Items["arguments"];
+
+            if (parameters != null)
+            {
+                if (parameters.ContainsKey("searchBy"))
+                {
+                    personsController.ViewData["CurrentSearchBy"] = Convert.ToString(parameters["searchBy"]);
+                }
+
+                if (parameters.ContainsKey("searchString"))
+                {
+                    personsController.ViewData["CurrentSearchString"] = Convert.ToString(parameters["searchString"]);
+                }
+
+                if (parameters.ContainsKey("sortBy"))
+                {
+                    personsController.ViewData["CurrentSortBy"] = Convert.ToString(parameters["sortBy"]);
+                }
+                else
+                {
+                    personsController.ViewData["CurrentSortBy"] = nameof(PersonResponse.PersonName);
+                }
+
+                if (parameters.ContainsKey("CurrentSortOrder"))
+                {
+                    personsController.ViewData["sortOrder"] = Convert.ToString(parameters["sortOrder"]);
+                }
+                else
+                {
+                    personsController.ViewData["sortOrder"] = nameof(SortOrderOptions.ASC);
+                }
+            }
+
+            personsController.ViewBag.SearchFields = new Dictionary<string, string>()
+            {
+                { nameof(PersonResponse.PersonName), "Person Name" },
+                { nameof(PersonResponse.Email), "Email" },
+                { nameof(PersonResponse.DateOfBirth), "Date of Birth" },
+                { nameof(PersonResponse.Gender), "Gender" },
+                { nameof(PersonResponse.CountryID), "Country" },
+                { nameof(PersonResponse.Address), "Address" },
+            };
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            // Log
+            _logger.LogInformation("{FilterName}.{MethodName} method", nameof(PersonsListActionFilter), nameof(OnActionExecuting));
+
+            context.HttpContext.Items["arguments"] = context.ActionArguments;
+
+            if (context.ActionArguments.ContainsKey("searchBy"))
+            {
+                string? searchBy = Convert.ToString(context.ActionArguments["searchBy"]);
+
+                // Validate the searchBy parameter value
+                if (!string.IsNullOrEmpty(searchBy))
+                {
+                    var searchOptions = new List<string>()
+                    {
+                        nameof(PersonResponse.PersonName),
+                        nameof(PersonResponse.Email),
+                        nameof(PersonResponse.DateOfBirth),
+                        nameof(PersonResponse.Gender),
+                        nameof(PersonResponse.CountryID),
+                        nameof(PersonResponse.Address)
+                    };
+
+                    // Reset the searchBy parameter value
+                    if (searchOptions.Any(temp => temp == searchBy) == false)
+                    {
+                        _logger.LogInformation("searchBy actual value {searchBy}", searchBy);
+                        context.ActionArguments["searchBy"] = nameof(PersonResponse.PersonName);
+                        _logger.LogInformation("searchBy updated value {searchBy}", context.ActionArguments["searchBy"]);
+                    }
+                }
+            }
+        }
+    }
+}
