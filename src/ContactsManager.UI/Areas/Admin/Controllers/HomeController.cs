d052937f21dd7afd5f9cd030680cf6241af37aa3
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-02-17 15:00:52 -0300

    feat: Add admin area

---
+++ src/ContactsManager.UI/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsManager.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/[controller]/[action]")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
