9760b835ca5ed5863ca9ad3f88f74af621ed33da
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-02-17 14:24:56 -0300

    Add UserTypeOptions to be used in register user

---
+++ src/ContactsManager.Core/Enums/UserTypeOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactsManager.Core.Enums
+{
+    public enum UserTypeOptions
+    {
+        User, Admin
+    }
+}
