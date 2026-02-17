c6d8a084040ca74556af943ad66998e539d17b0a
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-02-17 14:29:31 -0300

    feat: Add AllowAnonymous attribute to Error action

--- src/ContactsManager.UI/Controllers/HomeController.cs
+++ src/ContactsManager.UI/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Diagnostics;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ContactsManager.Controllers
 {
     public class HomeController : Controller
     {
+        [AllowAnonymous]
         [Route("Error")]
         public IActionResult Error()
         {
