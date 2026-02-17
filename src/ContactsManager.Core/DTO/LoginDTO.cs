23c38e47e35dd7d911d11eb1e01f77066370ee1d
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-02-17 14:24:26 -0300

    feat: Add LoginDTO and RegisterDTO to be used in controllers and views

---
+++ src/ContactsManager.Core/DTO/LoginDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ContactsManager.Core.DTO
+{
+    public class LoginDTO
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+    }
+}
---
+++ src/ContactsManager.Core/DTO/RegisterDTO.cs
@@ -0,0 +1,36 @@
+﻿using ContactsManager.Core.Enums;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ContactsManager.Core.DTO
+{
+    public class RegisterDTO
+    {
+        [Required(ErrorMessage = "Person name is required.")]
+        public string? PersonName { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        [Remote(action: "IsEmailAlreadyRegistered", controller: "Account", ErrorMessage = "Email already exists.")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Phone is required.")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Invalid phone number format.")]
+        [DataType(DataType.PhoneNumber)]
+        public string? Phone { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required.")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string? ConfirmPassword { get; set; }
+
+        public UserTypeOptions UserType { get; set; } = UserTypeOptions.User;
+    }
+}
