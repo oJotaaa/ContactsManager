7a3a8844ae3649dc6c1d9afd7093b27b5119e42f
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-02-17 14:31:58 -0300

    chore: Config Authorization and Identity services

--- src/ContactsManager.UI/StartupExtensions/ConfigureServicesExtension.cs
+++ src/ContactsManager.UI/StartupExtensions/ConfigureServicesExtension.cs
@@ -1,5 +1,9 @@
-﻿using ContactsManager.Filters.ActionFilters;
+﻿using ContactsManager.Core.Domain.IdentityEntities;
+using ContactsManager.Filters.ActionFilters;
 using Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Repositories;
 using RepositoryContracts;
@@ -18,9 +22,11 @@ namespace ContactsManager
             // Add services into IoC container
             services.AddScoped<ICountriesRepository, CountriesRepository>();
             services.AddScoped<IPersonsRepository, PersonsRepository>();
+
             services.AddScoped<ICountriesGetterService, CountriesGetterService>();
             services.AddScoped<ICountriesAdderService, CountriesAdderService>();
             services.AddScoped<ICountriesUploaderService, CountriesUploaderService>();
+
             services.AddScoped<IPersonsGetterService, PersonsGetterService>();
             services.AddScoped<PersonsGetterServiceWithFewExcelFields, PersonsGetterServiceWithFewExcelFields>();
             services.AddScoped<IPersonsAdderService, PersonsAdderService>();
@@ -35,6 +41,31 @@ namespace ContactsManager
                     options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
                 });
             }
+
+            // Enable Identity
+            services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+            {
+                options.Password.RequiredLength = 5;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireDigit = false;
+                options.Password.RequiredUniqueChars = 3;
+            })
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders()
+                .AddUserStore<UserStore<ApplicationUser, ApplicationRole, ApplicationDbContext, Guid>>()
+                .AddRoleStore<RoleStore<ApplicationRole, ApplicationDbContext, Guid>>();
+
+            services.AddAuthorization(options =>
+            {
+                options.FallbackPolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+            });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/Account/Login";
+            });
 
             services.AddHttpLogging(options =>
             {
