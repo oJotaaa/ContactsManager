17d71c621d9930e1deec7abccdd7d03509eacf00
João Felipe Fernandes Pimentel <joaofelipefernandes3221@gmail.com>
2026-02-17 14:23:15 -0300

    feat: Add IdentityEntities to User and Role

---
+++ src/ContactsManager.Core/Domain/IdentityEntities/ApplicationRole.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactsManager.Core.Domain.IdentityEntities
+{
+    public class ApplicationRole : IdentityRole<Guid>
+    {
+
+    }
+}
---
+++ src/ContactsManager.Core/Domain/IdentityEntities/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactsManager.Core.Domain.IdentityEntities
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+        public string? PersonName { get; set; }
+    }
+}
